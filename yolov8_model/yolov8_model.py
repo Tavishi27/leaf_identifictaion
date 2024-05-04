fe21f8a23b0584aeb4146f0738b4ebc8cc8a0d86
Tavishi27 <124404644+Tavishi27@users.noreply.github.com>
2024-05-03 17:24:15 -0700

    Create yolov8_model.py

---
+++ yolov8_model/yolov8_model.py
@@ -0,0 +1,10 @@
+'''
+5/3/2024
+Trains an image classification model using YOLO v8
+'''
+
+from ultralytics import YOLO
+# use a partially pre-trained model
+model = YOLO("yolov8n-cls.pt")
+# train the model using the data in the split_data folder
+model.train(data="/Users/tavis/OneDrive/Desktop/Leaf Identification/split_data", epochs=1, imgsz=64)
