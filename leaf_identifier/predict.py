e1f2bb365b49ea09bf7b9d37b61f63398cdcd901
Tavishi27 <124404644+Tavishi27@users.noreply.github.com>
2024-05-17 17:11:20 -0700

    Add files via upload

---
+++ leaf_identifier/predict.py
@@ -0,0 +1,41 @@
+'''
+Tavishi Bhatia
+5/7/24
+
+Program uses a trained YOLO v8 image classification model to classify provided leaf images
+'''
+
+from ultralytics import YOLO
+import pandas as pd
+
+
+# classify() takes an image path and uses a trained model to classify the image
+# and return's a prediction of the image's scientific name, common name, and confidence percentage
+def classify(img):
+    # load the trained model
+    model = YOLO("./train4/weights/last.pt")
+    # img = "/Users/tavis/OneDrive/Desktop/Leaf Identification/split_data/val/acer_saccharinum/13001158006214.jpg"
+    results = model(img)
+
+    # get the probabilities of the image fitting into that category for each leaf
+    names_dict = results[0].names
+    probs = results[0].probs.tolist()
+    # get the species with the highest probability
+    max_prob = 0
+    max_prob_index = 0
+    for n in range(0, len(probs)):
+        if probs[n] > max_prob:
+            max_prob = probs[n]
+            max_prob_index = n
+    # use leaves.csv to get the tree's common name
+    common_names = pd.read_csv("leaves.csv")
+    # format the scientific name
+    scientific_name = names_dict[max_prob_index].replace("_", " ").capitalize()
+    common_name = common_names[common_names["species"] == scientific_name].common_name.to_list()[0]
+    # convert the probability to a percentage
+    percent = round(max_prob * 100, 2)
+    # print(scientific_name + " (" + common_name + ")")
+    # print(str(percent) + "%")
+
+    return scientific_name, common_name, percent
+
